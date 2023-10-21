4382225bfa5083e5fe06119f032924efcbbae6f8
ezqHerrera <rodherrera14@gmail.com>
2023-10-20 21:07:12 -0300

    estructura básica del Santo Grial

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Header from './components/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="santoGrial">
+        <header><h1>Sobre el idioma Español</h1></header>
+        <div className='santoGrialBody'>
+
+          <nav>
+            <p>Лорем ипсум долор сит амет, меа ат фугит нолуиссе. Доценди анциллае ассентиор еи нец, легендос интеллегам ех яуо. Яуо цу агам воцибус доценди, пер поссе порро елецтрам ет, ан иус граецо латине омиттам. Вениам регионе те иус, но сед яуис еним. Яуо вирис дицунт делицатиссими не. Аппетере луцилиус суавитате усу цу, меа ад дицо веро граеце. Либер нолуиссе волуптатум яуи ад.</p>
+          </nav>
+
+          <main>
+            <h3>El idioma español, también conocido como castellano, es una de las lenguas más habladas del mundo y posee un gran significado cultural e histórico. A continuación se da un pantallazo del idioma.</h3>
+          </main>
+
+          <aside>
+            <p>Λορεμ ιπσθμ δολορ σιτ αμετ, cιβο δολορεσ τε vισ. Εθ ιθσ διcαντ ηενδρεριτ εφφιcιενδι, αεqθε προδεσσετ δισπθτανδο αδ ιθσ. Ποστεα εξπετενδα ιντελλεγεβατ προ εα. Ετ ιριθρε θταμθρ vιμ, εθμ πθτεντ μενανδρι σιγνιφερθμqθε ιδ, νεc εα ιθδιcο ατομορθμ. Εα σολθμ τατιον εφφιcιαντθρ εστ.</p>
+          </aside>
+
+        </div>
+        <footer>
+          <h6>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae a cum molestias consequatur non pariatur laborum labore recusandae harum saepe esse, fugit quam eum dolorem fuga nesciunt, quasi animi sit.</h6>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css';
+
+const Header = (props) => {
+    return (
+        <>
+            <header>
+                <h1>{props.titulo}</h1>
+            </header>
+        </>
+    )
+}
+
+export default Header;
